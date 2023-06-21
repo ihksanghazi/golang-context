e6b07800dba163964ffb0a364ec95c6eb45004eb
Nursandy Ihksan <nursandyihksan97@gmail.com>
2023-06-21 14:09:55 +0700

    contextWithValue

--- context_test.go
+++ context_test.go
@@ -13,3 +13,27 @@ func TestContext(t *testing.T) {
 	todo:= context.TODO()
 	fmt.Println(todo)
 }
+
+func TestContextWithValue(t *testing.T) {
+	contextA:=context.Background()
+
+	contextB:=context.WithValue(contextA,"b","B")
+	contextC:=context.WithValue(contextA,"c","C")
+
+	contextD:=context.WithValue(contextB,"d","D")
+	contextE:=context.WithValue(contextB,"e","E")
+
+	contextF:=context.WithValue(contextC,"f","F")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
+
+	fmt.Println(contextF.Value("f")) // dapat
+	fmt.Println(contextF.Value("c")) // dapat milik parent
+	fmt.Println(contextF.Value("b")) // tidak dapat, beda parent
+	fmt.Println(contextA.Value("b")) // tidak bisa mengambil data child
+}
